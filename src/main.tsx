3317aa4ec284fde2b66c29d9a4afa4e9696c6308
EricLicea-Projects <elicea.2000@gmail.com>
2025-02-06 23:25:51 -0800

    Downgrade ChakraUI V2

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { Provider } from "@/components/ui/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>
 );
