208feaec0531c93528a349ce0af65f970fafe9ae
EricLicea-Projects <elicea.2000@gmail.com>
2025-02-07 23:50:34 -0800

    Color Mode Switch done

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { IconButton, useColorMode } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  return (
+    <IconButton
+      colorScheme={isDark ? "orange" : "gray"}
+      isRound={true}
+      aria-label={`Switch to ${isDark ? "light" : "dark"} mode`}
+      size={"md"}
+      fontSize={"30px"}
+      variant="ghost"
+      onClick={toggleColorMode}
+      icon={isDark ? <FaSun /> : <FaMoon />}
+    />
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} paddingRight={"3px"}>
       <Image src={logo} boxSize={"70px"} />
       <Text fontSize={"2xl"}>Guessamon</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
